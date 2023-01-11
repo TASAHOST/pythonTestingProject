e662c2af6e9450544282e1c8de976e5daf4ed873
TASAHOST <120707750+TASAHOST@users.noreply.github.com>
2023-01-11 14:38:38 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+s = Service('D:\\webdriver\\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+driver.get("https://sc.npru.ac.th/")
+time.sleep(10)
+driver.close()
